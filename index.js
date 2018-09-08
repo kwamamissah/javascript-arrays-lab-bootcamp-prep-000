9e5a34e02b073532d5b2f89be42be1962ddd657c
kwamamissah@gmail.com <kwamamissah@gmail.com>
2018-09-08 18:31:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendKitten(name) {
+ return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+ return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(name) {
+ return kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name) {
+ return kittens.shift(name);
+}
